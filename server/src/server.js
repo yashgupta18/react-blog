706f32d8b4d2c08476ea59b8348041b9ef1dd46a
Yash Gupta <18guptayash@gmail.com>
2021-07-31 13:00:35 +0530

    API endpoints for GET and POST comments

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,56 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 
-const articlesInfo = {
-  "learn-react": {
-    comments: [],
-  },
-  "learn-node": {
-    comments: [],
-  },
-  "my-thought-on-learning-react": {
-    comments: [],
-  },
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db("myblog");
+    await operations(db);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "wrror connecting to DB" });
+  }
 };
+
+//Get Requests for comments of a blog
+app.get("/api/articles/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
+});
 
 app.use(express.json());
 
+//post username, comments in db
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
-  articlesInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articlesInfo[articleName]);
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(8000, () => console.log("Listening on PORT 8000"));
